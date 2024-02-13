a85ec276a00c32a5383901d591e9f2df2862142d
RedaRahmani <mohamedreda.rahmani20@ump.ac.ma>
2024-02-13 23:20:06 +0100

    add MVC Architecture

---
+++ app.js
@@ -0,0 +1,10 @@
+// app.js
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.use(express.json()); // Enable JSON parsing
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
