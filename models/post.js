9abd5f1c6e50fbac6acb70a2e7cd4548d765a29b
RedaRahmani <mohamedreda.rahmani20@ump.ac.ma>
2024-02-14 00:19:15 +0100

    add Read Write in models

--- models/post.js
+++ models/post.js
@@ -1,2 +1,19 @@
 const fs = require('fs');
 const path = require('path');
+const { stringify } = require('querystring');
+
+const dataPath = '../blogs.json'
+
+const readData = () => {
+    const rawData = fs.readFileSync(dataPath);
+    return JSON.parse(rawData);
+    };
+const getAllPosts = () => {
+    return readData();
+    };
+
+const writeData = (data) => {
+    fs.writeFileSync(dataPath, stringify(data , null , 2))
+}
+
+
