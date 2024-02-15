ca0670e4763c1667a41c283aa7dfd1edf511f7b4
Fatima MAAYANE <fatimamaayane@gmail.com>
2024-02-15 15:23:57 +0100

    add logging feature

--- controllers/loginController.js
+++ controllers/loginController.js
@@ -1,6 +1,7 @@
-import express from "express";
-import bcrypt from "bcrypt";
 
+const express = require('express');
+const fs = require('fs');
+const bcrypt = require('bcrypt');
 const app = express();
 const port = 3000;
 
@@ -9,26 +10,49 @@ const users = [];
 
 app.use(express.json());
 
+const USERS_FILE_PATH = 'users.json';
+
+// Function to read users from the JSON file
+const readUsersFromFile = () => {
+    try {
+        const data = fs.readFileSync(USERS_FILE_PATH, 'utf8');
+        return JSON.parse(data);
+    } catch (error) {
+        return [];
+    }
+};
+
+// Function to write users to the JSON file
+const writeUsersToFile = (users) => {
+    fs.writeFileSync(USERS_FILE_PATH, JSON.stringify(users, null, 2), 'utf8');
+};
 
 app.post("/login", async (req, res) => {
     try {
         const { email, password } = req.body;
+
+        // Read users from the file
+        const usersFromFile = readUsersFromFile();
+
         // Find user
-        const findUser = users.find((data) => email == data.email);
+        const findUser = usersFromFile.find((data) => email === data.email);
+
         if (!findUser) {
-            res.status(400).send("Wrong email or password !");
+            return res.status(400).send("Wrong email or password!");
         }
+
         const passwordMatch = await bcrypt.compare(password, findUser.password);
-        if(passwordMatch) {
-            res.status(200).send("Logged in successfully!")
+
+        if (passwordMatch) {
+            return res.status(200).send("Logged in successfully!");
         } else {
-            res.status(400).send("Wrong email or password !");
+            return res.status(400).send("Wrong email or password!");
         }
     } catch (err) {
-    res.status(500).send({ message: err.message });
+        return res.status(500).send({ message: err.message });
     }
 });
 
 app.listen(port, () => {
-    console.log("Server is started on port 3000");
+    console.log(`Server is started on port ${port}`);
 });
